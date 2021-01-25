0142a9f6f4d07f257c83e253a88926db6162ce8e
AlexStolbov <stolbov_alexey@mail.ru>
2021-01-25 16:56:27 +0300

    step two

--- hexlet_python_package/scripts/gendiff.py
+++ hexlet_python_package/scripts/gendiff.py
@@ -6,6 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', metavar='FORMAT', help='set format of output')
     args = parser.parse_args()
     first_file = args.first_file
     second_file = args.second_file

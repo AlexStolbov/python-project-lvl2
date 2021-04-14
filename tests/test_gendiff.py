03c6679c3e64d97abf52359b36164ffcc4c2217d
AlexStolbov <stolbov_alexey@mail.ru>
2021-04-14 14:50:01 +0300

    refactor test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -14,21 +14,25 @@ def path_current(*paths):
     return path.join(CURRENT_DIR, 'fixtures', *paths)
 
 
+SET_TESTING_DATA = [
+    (PLAIN_JSON_DIR, 'plain_original.json', 'plain_modified.json',
+     'stylish_plain_diff.txt', 'stylish'),
+    (PLAIN_YML_DIR, 'plain_original.yml', 'plain_modified.yml',
+     'stylish_plain_diff.txt', 'stylish'),
+    (NESTED_JSON_DIR, 'nested_original.json', 'nested_modified.json',
+     'stylish_nested_diff.txt', 'stylish'),
+    (NESTED_YML_DIR, 'nested_original.yml', 'nested_modified.yml',
+     'stylish_nested_diff.txt', 'stylish'),
+    (NESTED_JSON_DIR, 'nested_original.json', 'nested_modified.json',
+     'plain_diff.txt', 'plain'),
+    (NESTED_JSON_DIR, 'nested_original.json', 'nested_modified.json',
+     'json_diff.json', 'json')
+    ]
+
+
 @pytest.mark.parametrize(
     'files_dir, path_origin, path_modified, path_diff, out_format',
-    [(PLAIN_JSON_DIR, 'plain_original.json', 'plain_modified.json',
-      'stylish_plain_diff.txt', 'stylish'),
-     (PLAIN_YML_DIR, 'plain_original.yml', 'plain_modified.yml',
-      'stylish_plain_diff.txt', 'stylish'),
-     (NESTED_JSON_DIR, 'nested_original.json', 'nested_modified.json',
-      'stylish_nested_diff.txt', 'stylish'),
-     (NESTED_YML_DIR, 'nested_original.yml', 'nested_modified.yml',
-      'stylish_nested_diff.txt', 'stylish'),
-     (NESTED_JSON_DIR, 'nested_original.json', 'nested_modified.json',
-      'plain_diff.txt', 'plain'),
-     (NESTED_JSON_DIR, 'nested_original.json', 'nested_modified.json',
-      'json_diff.json', 'json')
-     ])
+    SET_TESTING_DATA)
 def test_plain_json(files_dir, path_origin, path_modified, path_diff,
                     out_format):
     res = generate_diff(path_current(files_dir, path_origin),
